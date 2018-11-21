20092d6d7d09aa389ae8376e391addba569669a1
Eduardo Acacio <acaciosh@gmail.com>
2018-11-21 16:22:11 -0300

    adicionando novos campos do currículo

--- src/pages/curriculo.js
+++ src/pages/curriculo.js
@@ -3,16 +3,16 @@ import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 
-const Projects = () => (
+const Curriculo = () => (
   <Layout>
     <section className="section is-size-5-desktop is-size-6-touch">
-      <h1 className="title has-text-light has-text-centered">Curriculo</h1>
+      <h1 className="title has-text-light has-text-centered">Currículo</h1>
         <div className="container">
             <section className="section is-size-4 is-size-5-touch">
                 <div className="container content">
                     <h4 className="title has-text-light">Carlos Eduardo Acacio Carneiro{' '} <span role="img" aria-label="pc graficos e bandeira"></span></h4>
                     <p>
-                    acaciosh@gmail.com | +55 85 986778994 | Fortaleza, CE, Brasil
+                    acaciosh@gmail.com | +55 85 98677.8994 | Fortaleza, CE - Brasil
                     </p>
                     <p>
                     Profissional com 4 anos de experiência no campo de Desenvolvimento de Sistemas, trabalhei em vários tipos de
@@ -35,6 +35,115 @@ const Projects = () => (
                     Último Semestre – <a href="http://flf.edu.br/">(http://flf.edu.br/)</a>
                     </p>
                 </div>
+            </section>
+            <hr />
+            <section className="section is-size-4 is-size-5-touch">
+                <b>Conhecimentos e Habilidades</b><br /> <br />
+                
+                Escala
+                <table className="table is-bordered is-size-7 is-size-8-touch"> 
+                    <thead>
+                        <tr> 
+                            <th>Básico</th>
+                            <th>Intermediário</th>
+                            <th>Avançado</th>
+                        </tr>
+                    </thead>
+                    <tr> 
+                        <td>*</td>
+                        <td>**</td>
+                        <td>***</td>
+                    </tr>
+                </table>
+                
+                Idiomas
+                <table className="table is-bordered is-size-7 is-size-8-touch"> 
+                    <thead> 
+                    <tr> 
+                        <th>Espanhol</th>
+                        <th>Inglês</th>
+                    </tr>
+                    </thead>
+                    <tr> 
+                        <td>Escrita *</td>
+                        <td>Escrita **</td>
+                    </tr>
+                    <tr> 
+                        <td>Leitura **</td>
+                        <td>Leitura **</td>
+                    </tr>
+                    <tr> 
+                        <td>Pronuncia *</td>
+                        <td>Pronuncia *</td>
+                    </tr>
+                </table>
+
+
+                Skills
+                <table className="table is-bordered is-size-7 is-size-8-touch"> 
+                    <thead>
+                        <tr> 
+                            <th>Linguagens</th>
+                            <th>Frameworks</th>
+                            <th>Bancos de Dados</th>
+                        </tr>
+                    </thead>
+                    <tr> 
+                        <td>C# .Net ***</td>
+                        <td>.NET **</td>
+                        <td>Sybase ***</td>
+                    </tr>
+                    <tr> 
+                        <td>Java **</td>
+                        <td>ASP .NET MVC ***</td>
+                        <td>SQL Server ***</td>
+                    </tr>
+                    <tr> 
+                        <td>T/SQL ***</td>
+                        <td>ASP .NET MEB API ***</td>
+                        <td>Oracle **</td>
+                    </tr>
+                    <tr> 
+                        <td>JavaScript **</td>
+                        <td>ASP .NET Core ***</td>
+                        <td>MySQL ***</td>
+                    </tr>
+                    <tr> 
+                        <td>Angular **</td>
+                        <td>Entity Framework ***</td>
+                        <td>MongoDB *</td>
+                    </tr>
+                    <tr> 
+                        <td>Node **</td>
+                        <td>Identity **</td>
+                        <td></td>
+                    </tr>
+                </table>
+
+                Devops 
+                <table className="table is-bordered is-size-7 is-size-8-touch"> 
+                    <thead>
+                        <tr> 
+                            <th>Git</th>
+                            <th>GitHub</th>
+                            <th>Bitbucket</th>
+                            <th>VSTS</th>
+                            <th>Azure</th>
+                            <th>AWS</th>
+                            <th>Docker</th>
+                        </tr>
+                    </thead>
+                    <tr> 
+                        <td>***</td>
+                        <td>***</td>
+                        <td>***</td>
+                        <td>**</td>
+                        <td>**</td>
+                        <td>*</td>
+                        <td>*</td>
+                    </tr>
+                </table>
+
             </section>
             <hr />
             <section className="section is-size-4 is-size-5-touch">
@@ -98,4 +207,4 @@ const Projects = () => (
   </Layout>
 );
 
-export default Projects;
+export default Curriculo;
